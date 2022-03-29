ba3cdf02887e5bbdf68f8a850e52c964809a5289
Kevin Kandlbinder <kevin@kevink.dev>
2022-03-29 16:01:18 +0200

    webui: Handle small screens

--- webui/src/layouts/PanelLayout.tsx
+++ webui/src/layouts/PanelLayout.tsx
@@ -1,18 +1,21 @@
 import React, {useState} from "react";
 
 import {Link, NavLink, useOutlet} from "react-router-dom";
-import {Home, List, ClipboardList, ExternalLink, ChevronRight, MessageSquare, LogOut} from "lucide-react";
+import {Home, List, ClipboardList, BookOpen, ChevronRight, MessageSquare, LogOut} from "lucide-react";
+import { Squash as Hamburger } from 'hamburger-react';
 
 import {ReactComponent as Logo} from "../logo.svg";
 
 import styles from "./PanelLayout.module.scss";
-import {Trans} from "react-i18next";
+import {Trans, useTranslation} from "react-i18next";
 import {useAppDispatch} from "../app/hooks";
 import {logOut} from "../features/auth/authSlice";
 
 const PanelLayout = () => {
+    const {t} = useTranslation()
     const outlet = useOutlet();
     const [hashingExpanded, setHashingExpanded] = useState(false)
+    const [sideNavExpanded, setSideNavExpanded] = useState(true)
 
     const dispatch = useAppDispatch()
 
@@ -20,20 +23,24 @@ const PanelLayout = () => {
         <a href={"#main"} className={styles.skipToContent}><Trans i18nKey={"panel:jump_to_content"}>Jump to Content</Trans></a>
         <a href={"#navigation"} className={styles.skipToContent}><Trans i18nKey={"panel:jump_to_navigation"}>Jump to Navigation</Trans></a>
         <div className={styles.topBar}>
+            <div className={styles.hamburger}>
+                <Hamburger toggle={setSideNavExpanded} toggled={sideNavExpanded} rounded={true} duration={.25} distance={"lg"} />
+            </div>
             <Link to={"/"} className={styles.logo}><Logo/> <span>Matrix-Veles</span></Link>
-            <a href={"https://veles.1in1.net/docs/intro"} target={"_blank"} rel={"noreferrer"}><ExternalLink/> <span><Trans i18nKey={"panel:documentation"}>Documentation</Trans></span></a>
-            <button onClick={() => {dispatch(logOut())}}><LogOut/> Logout</button>
+            <a href={"https://veles.1in1.net/docs/intro"} target={"_blank"} rel={"noreferrer"} title={t("panel:documentation", "Documentation")}><BookOpen/><span> <Trans i18nKey={"panel:documentation"}>Documentation</Trans></span></a>
+            <button onClick={() => {dispatch(logOut())}} title={t("panel:logout", "Logout")}><LogOut/><span> <Trans i18nKey={"panel:logout"}>Logout</Trans></span></button>
         </div>
         <div className={styles.content}>
-            <nav id={"navigation"}>
-                <NavLink to={"/"}><Home/><span><Trans i18nKey={"panel:navigation.dashboard"}>Dashboard</Trans></span></NavLink>
-                <NavLink to={"/rooms"}><MessageSquare/><span><Trans i18nKey={"panel:navigation.rooms"}>My Rooms</Trans></span></NavLink>
+            <div className={styles.navUnderlay + (sideNavExpanded ? " "+styles.show : "")} onClick={() => setSideNavExpanded(false)} />
+            <nav id={"navigation"} className={sideNavExpanded ? styles.expanded : ""}>
+                <NavLink to={"/"} onClick={() => setSideNavExpanded(false)}><Home/><span><Trans i18nKey={"panel:navigation.dashboard"}>Dashboard</Trans></span></NavLink>
+                <NavLink to={"/rooms"} onClick={() => setSideNavExpanded(false)}><MessageSquare/><span><Trans i18nKey={"panel:navigation.rooms"}>My Rooms</Trans></span></NavLink>
                 <div className={styles.dropdown + (hashingExpanded?" "+styles.expanded:"")}>
                     <button onClick={() => setHashingExpanded(!hashingExpanded)}>
                         <ChevronRight/> <span><Trans i18nKey={"panel:navigation.hash_checker"}>Hash-Checker</Trans></span>
                     </button>
-                    <NavLink to={"/hashing/lists"}><List/><span><Trans i18nKey={"panel:navigation.hash_lists"}>Lists</Trans></span></NavLink>
-                    <NavLink to={"/hashing/entries"}><ClipboardList/><span><Trans i18nKey={"panel:navigation.hash_entries"}>Entries</Trans></span></NavLink>
+                    <NavLink to={"/hashing/lists"} onClick={() => setSideNavExpanded(false)}><List/><span><Trans i18nKey={"panel:navigation.hash_lists"}>Lists</Trans></span></NavLink>
+                    <NavLink to={"/hashing/entries"} onClick={() => setSideNavExpanded(false)}><ClipboardList/><span><Trans i18nKey={"panel:navigation.hash_entries"}>Entries</Trans></span></NavLink>
                 </div>
             </nav>
             <main id={"main"}>
